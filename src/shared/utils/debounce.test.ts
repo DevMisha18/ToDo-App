c5902fb7c042fb85aa106487fd5e641254dc863a
DevMisha18 <misha6402041@gmail.com>
2025-08-10 17:45:23 +0200

    refactor: rewrite tests to use fake timers

--- src/shared/utils/debounce.test.ts
+++ src/shared/utils/debounce.test.ts
@@ -1,35 +1,38 @@
 import { debounce } from "./debounce";
 
-function wait(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 describe("debounce", () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   it("should not execute the functoin immediatly", async () => {
     const fakeFn = jest.fn();
-    const delay = 500;
+    const delay = 300;
     const debouncedFakeFn = debounce(fakeFn, delay);
 
     debouncedFakeFn();
     expect(fakeFn).toHaveBeenCalledTimes(0);
 
-    await wait(500);
+    jest.advanceTimersByTime(500);
     expect(fakeFn).toHaveBeenCalledTimes(1);
   });
 
   it("should hault execution if called multiple times", async () => {
     const fakeFn = jest.fn();
-    const delay = 500;
+    const delay = 300;
     const debouncedFakeFn = debounce(fakeFn, delay);
 
     debouncedFakeFn();
     expect(fakeFn).toHaveBeenCalledTimes(0);
 
-    await wait(200);
+    jest.advanceTimersByTime(100);
     debouncedFakeFn();
     expect(fakeFn).toHaveBeenCalledTimes(0);
 
-    await wait(500);
+    jest.advanceTimersByTime(500);
     expect(fakeFn).toHaveBeenCalledTimes(1);
   });
 });
